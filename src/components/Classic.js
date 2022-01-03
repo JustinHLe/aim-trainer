c8aa9a0152f6bdefbb737c0042cea17ca5453876
JustinHLe <jle043@ucr.edu>
2022-01-03 15:59:03 -0800

    add basic functionality

--- src/components/Classic.js
+++ src/components/Classic.js
@@ -4,37 +4,34 @@ import { useState, useEffect } from 'react';
 
 function Classic (){
     const [hit, setHit] = useState(0)
-    var intervalInProgress = false
     var timer = null
+    const hitSound = new Audio("/hit.wav")
     useEffect(()=>{
-        timer = setInterval(moveSquare, 2000)
-    },[])
+        clearInterval(timer)
+        timer = setInterval(moveSquare, 1000)
+    },[hit])
     function handleHit(){
+        hitSound.play()
         setHit(hit+1)
         resetSquare()
     }
     function resetSquare(){
-        moveSquare()
         clearInterval(timer)
-        timer = setInterval(moveSquare, 2000)
+        moveSquare()
     }
 
     function moveSquare(){
-        if (intervalInProgress){
-            return 
-        } else {
-            const gameContainer = document.getElementById("game-container")
-            const height = gameContainer.offsetHeight
-            const width = gameContainer.offsetWidth
-            const square = document.getElementById("square")
-            square.style.top = (Math.random() * (height - 100)) + "px";
-            square.style.left = (Math.random() * (width - 100)) + "px";
-        }
+        const gameContainer = document.getElementById("game-container")
+        const height = gameContainer.offsetHeight
+        const width = gameContainer.offsetWidth
+        const square = document.getElementById("square")
+        square.style.top = (Math.random() * (height - 100)) + "px";
+        square.style.left = (Math.random() * (width - 100)) + "px";
     }
     return (
         <>
         <section className='game-section'>
-            <div>
+            <div className='counter'>
                 {hit}
             </div>
             <div className='game-container' id = "game-container">
